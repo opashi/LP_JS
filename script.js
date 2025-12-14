39d330885fe1ad318bd01c39a23e5afb13143b71
opashi <yuuki.0710.baseball@i.softbank.jp>
2025-12-14 23:49:38 +0900

    update ver1.6.5 - splash screen added

--- script.js
+++ script.js
@@ -33,3 +33,9 @@ document.querySelectorAll('.profile').forEach(card => {
     this.classList.toggle('active');
   });
 });
+
+// ▼ スプラッシュスクリーン
+$(window).on('load', function() {
+  $('#splash').delay(1500).fadeOut('slow');
+  $('#splash-logo').delay(1200).fadeOut('slow');
+});
